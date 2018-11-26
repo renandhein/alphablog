830c9d209eb540b9653f71a6d9e8095f1961560b
Renan Dhein <renan.dhein@gmail.com>
2018-11-26 16:03:13 -0200

    Finishing up colorpicker component

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,12 @@ module ApplicationHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: "img-circle")
   end
+
+  def preferred_color
+    if (current_user && current_user.color.present?)
+      current_user.color
+    else
+      '#fff'
+    end
+  end
 end
